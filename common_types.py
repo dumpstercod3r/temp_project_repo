98cf9c85c1c3b3aa09aee9dc101415e9da79e542
dumpstercod3r <hhreblora@up.edu.ph>
2026-05-30 21:22:49 +0800

    Update common_types.py
    finished model, awaiting testing

--- common_types.py
+++ common_types.py
@@ -3,7 +3,54 @@
 from enum import StrEnum, auto
 from typing import Protocol
 
-from model import Coord, Node
+
+type Coord = tuple[int, int]
+type Graph = list[Node]
+"""
+There are two types of Grids, Ally Grid and Obstacle Grid.
+Ally Grid is a Grid containing either Shooter | Tower | None.
+Members of Ally Grid can be placed anywhere except on enemy path.
+Obstacle Grid is a Grid containing either Tunnels | Buildings | None.
+Members of Obstacle Grid can be placed anywhere.
+"""
+type Grid = list[list[GridConstruct | None]]
+
+class Node:
+    """
+    Nodes for graph-based traversal.
+    Nodes contain info on what enemy is on it and the other nodes the enemy can go to.
+    Superimposed on Ally Grid.
+    """
+    def __init__(self, x: int, y: int):
+        self._coords: Coord = (x, y)
+        self._occupant: Enemy | None = None
+        self._connections: list[Node] = []
+    
+    @property
+    def coords(self) -> Coord:
+        return self._coords
+    
+    @property
+    def occupant(self) -> Enemy | None:
+        return self._occupant
+    
+    @property
+    def is_occupied(self) -> bool:
+        return self._occupant is not None
+    
+    @property
+    def connections(self) -> list[Node]:
+        return self._connections
+
+    def occupy(self, enemy: Enemy):
+        self._occupant = enemy
+    
+    def vacate(self):
+        self._occupant = None
+    
+    def set_connections(self, *nodes: Node):
+        self._connections.extend(nodes)
+
 
 class Color(StrEnum):                                                                   # There will be custom colors
     GREEN = auto()
@@ -85,7 +132,7 @@ class Enemy(Protocol):
     @property
     def exp(self) -> int:
         ...
-    def move(self, node: Node):
+    def move_to_node(self, node: Node):
         ...
     def valid_shot(self, damage: int):
         ...
