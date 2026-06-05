abe300e03b6302e47a720f6978826aa435dc1528
dumpstercod3r <hhreblora@up.edu.ph>
2026-06-05 20:36:21 +0800

    Update model_phase1.py

--- phase1/model_phase1.py
+++ phase1/model_phase1.py
@@ -17,6 +17,7 @@ class ZumaModelPhase1:
         self._bullet_manager: BulletManager = BulletManager(self._colors)
         self._rng: Random = Random(67)
         self._bullet_colors_queue: list[Color] = self._rng.sample(self._colors, k=len(self._colors))
+        self._is_game_over: bool = False
     
     @property
     def shooter(self) -> Shooter:
@@ -41,6 +42,10 @@ class ZumaModelPhase1:
     @property
     def next_bullet_color(self) -> Color:
         return self._bullet_colors_queue[0]
+
+    @property
+    def is_game_over(self) -> bool:
+        return self._is_game_over
     
     def shoot(self, x: int, y: int, angle: int):
         self._bullet_manager.create_bullet(self.shooter.bullet_type, self.pop_next_bullet_color(), x, y, angle)
@@ -63,7 +68,9 @@ class ZumaModelPhase1:
         self._enemy_manager.prepare_round()
     
     def update(self):
-        ...
+        if not self.is_game_over:
+            self._enemy_manager.update() # I dunno how to do time shit
+            self._bullet_manager.update()
     
     def end_round(self):
         ...
